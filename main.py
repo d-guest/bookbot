b42733fc748c6a2b0c9054e67a0b056ad3bcbc5a
Dylan Guest <dguest3@gmail.com>
2025-09-28 17:23:37 -0500

    ch3: bookbot is finished!

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from stats import get_word_count, get_character_count
+import sys
+from stats import get_word_count, get_character_count, sort_characters_list, characters_to_list
 
 def get_book_text(filepath):
-    with open(f'{filepath}', 'r') as file:
+    with open(f"{filepath}", "r") as file:
         content = file.read()
     return content
 
@@ -10,7 +11,21 @@ def main(relative_path):
     book_text = get_book_text(relative_path)
     word_count = get_word_count(book_text)
     characters = get_character_count(book_text)
-    print(f'Found {word_count} total words')
-    print(characters)
-    
-main('books/frankenstein.txt')
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at " + relative_path + "...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("------- Character Count ---------")
+    char_list = characters_to_list(characters)
+    sorted_characters = sort_characters_list(char_list)
+    for item in sorted_characters:
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+            
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    main(sys.argv[1])
+
--- stats.py
+++ stats.py
@@ -9,3 +9,15 @@ def get_character_count(text):
         for char in word.lower():
             characters[char] = characters.get(char, 0) + 1
     return characters
+
+def characters_to_list(char_dict):
+    char_list = []
+    for char, count in char_dict.items():
+        if char.isalpha():  # Only include alphabetic characters
+            char_list.append({"char": char, "num": count})
+        else:
+            continue
+    return char_list
+
+def sort_characters_list(char_list):
+    return sorted(char_list, key=lambda x: x['num'], reverse=True)
